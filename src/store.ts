735a409901c0ed45200e1dc83387e0af11099e13
Omair Nabiel <nabiel.omair@gmail.com>
2022-11-19 14:09:42 +0500

    chore: add initial shape state

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,14 @@
 import { createStore } from "solid-js/store";
 
+import { Shapes } from "./types/shapes";
+
+// initial state
+const shape: Shapes = "line";
 const lineCap: CanvasLineCap = "round";
 
 export const [options, setOptions] = createStore({
-  lineWidth: 10,
+  shape,
   lineCap,
+  lineWidth: 10,
   strokeStyle: "black",
 });
--- src/types/shapes.ts
+++ src/types/shapes.ts
@@ -1,1 +1,1 @@
-export type shapes = "line" | "circle" | "rectangle";
+export type Shapes = "line" | "circle" | "square" | "rectangle" | "free-hand"
